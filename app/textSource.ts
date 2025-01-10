1e1e8262199fce20dcd6d30d95a31c94dc9176a7
m-ku66 <johanmarcmiango15@gmail.com>
2025-01-10 10:50:05 -0500

    Initial commit. Set up theme switching system and laid the groundwork for the language switching system. Imported fonts and installed all major dependencies

---
+++ app/textSource.ts
@@ -0,0 +1,42 @@
+const pageText = {
+  en: {
+    main: {
+      1: "An all new beginning",
+      2: "2025",
+      3: "January",
+      4: "December",
+      5: "365",
+      6: "Days",
+      7: "let's go forward together. Make this one count",
+    },
+    desc: {
+      1: "2025 - A New Year",
+      2: "The present moment, a fresh start, a number. The what does the new year mean to you?",
+    },
+    echo: {
+      a: { 1: "Where", 2: "Will you go?" },
+      b: { 1: "What", 2: "Will you do?" },
+    },
+  },
+  ja: {
+    main: {
+      1: "新しい始まり",
+      2: "2025 年",
+      3: "1月",
+      4: "12月",
+      5: "365",
+      6: "日",
+      7: "一緒に前進しましょう。この一年を大切なものにしていきましょう",
+    },
+    desc: {
+      1: "新年2025明け",
+      2: "現在の瞬間、新たな始まり、そして数字。新年はあなたにとって何を意味しますか？",
+    },
+    echo: {
+      a: { 1: "どこに", 2: "行きますか？" },
+      b: { 1: "何を", 2: "しますか？" },
+    },
+  },
+};
+
+export default pageText;
